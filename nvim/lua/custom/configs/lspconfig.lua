7dd9fe2f9cbc2a3d87d795b1acaa4202d7cd0599
cheurte <corentin.heurte@gmail.com>
2023-06-13 16:36:49 +0200

    rust debugger added + correction lsp

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -3,12 +3,12 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = {"rust"},
-    root_dir = lspconfig.util.root_pattern("Cargo.toml")
-})
+-- lspconfig.rust_analyzer.setup({
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--     filetypes = {"rust"},
+--     root_dir = lspconfig.util.root_pattern("Cargo.toml")
+-- })
 
 require'lspconfig'.lua_ls.setup {
     on_attach = on_attach,
--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -88,6 +88,9 @@ local plugins = {
     end,
   },
   {
+    "mfussenegger/nvim-dap",
+  },
+  {
     "lervag/vimtex",
     ft = { "tex" },
     init = function()
@@ -103,15 +106,9 @@ local plugins = {
   },
   {
     "folke/drop.nvim",
-    -- event = "VimEnter",
     config = function()
       require("drop").setup()
     end,
-    -- config = function()
-    --     math.randomseed(os.time())
-    --     local theme = ({ "stars", "snow", "xmas" })[math.random(1, 3)]
-    --     require("drop").setup { theme = theme }
-    -- end,
   },
   {
     "chrisbra/csv.vim",
@@ -143,27 +140,11 @@ local plugins = {
         cyclic = true,
         -- whether the shada file is updated after modifying uppercase marks. default false
         force_write_shada = false,
-        -- how often (in ms) to redraw signs/recompute mark positions.
-        --  higher values will have better performance but may cause visual lag,
-        -- while lower values may cause performance penalties. default 150.
         refresh_interval = 250,
-        -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
-        -- marks, and bookmarks.
-        -- can be either a table with all/none of the keys, or a single number, in which case
-        -- the priority applies to all marks.
-        -- default 10.
         sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
-        -- disables mark tracking for specific filetypes. default {}
         excluded_filetypes = {},
-        -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
-        -- sign/virttext. Bookmarks can be used to group together positions and quickly move
-        -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
-        -- default virt_text is "".
         bookmark_0 = {
           sign = "⚑",
-          -- virt_text = "hello world",
-          -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
-          -- defaults to false.
           annotate = false,
         },
         mappings = {},
@@ -175,22 +156,12 @@ local plugins = {
     event = "InsertEnter",
     config = function()
       require("better_escape").setup {
-        mapping = { "jk", "jj" }, -- a table with mappings to use
-        timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-        clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-        keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+        mapping = { "jk", "jj" },
+        timeout = vim.o.timeoutlen,
+        clear_empty_lines = false,
+        keys = "<Esc>",
       }
     end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  },
-  {
-    "justinhj/battery.nvim",
-    requires = { { "kyazdani42/nvim-web-devicons" }, { "nvim-lua/plenary.nvim" } },
   },
   {
     "christoomey/vim-tmux-navigator",
