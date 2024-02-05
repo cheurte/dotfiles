0b4696a5acf4c561e29ca71bd6a4f222166ea570
Corentin Heurté <corentin.heurte@gmail.com>
2024-02-05 16:04:40 +0100

    corrections

--- custom/plugins/mason.lua
+++ custom/plugins/mason.lua
@@ -1,26 +1,28 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "codelldb",
-        "clangd",
-        "cmakelang",
-        "cmakelint",
-        "neocmakelsp",
-        "cmakelint",
-        "hadolint",
-        "json-lsp",
-        "marksman",
-        "markdownlint",
-        "pyright",
-        "ruff-lsp",
-        "debugpy",
-        "rust-analyzer",
-        "texlab",
-        "ltex-ls"
-      },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "codelldb",
+                "clangd",
+                "cmakelang",
+                "cmakelint",
+                "neocmakelsp",
+                "cmakelint",
+                "hadolint",
+                "dockerfile-language-server",
+                "docker-compose-language-service",
+                "json-lsp",
+                "marksman",
+                "markdownlint",
+                "pyright",
+                "ruff-lsp",
+                "debugpy",
+                "rust-analyzer",
+                "texlab",
+                "ltex-ls"
+            },
+        },
+        automatic_install = true,
     },
-    automatic_install = true,
-  },
 }
--- custom/plugins/python.lua
+++ custom/plugins/python.lua
@@ -1,23 +1,21 @@
 return {
-  {
-    "linux-cultist/venv-selector.nvim",
-    cmd = "VenvSelect",
-    opts = function(_, opts)
-      if require("lazyvim.util").has("nvim-dap-python") then
-        opts.dap_enabled = true
-      end
-      return vim.tbl_deep_extend("force", opts, {
-        name = {
-          "venv",
-          ".venv",
-          "env",
-          ".env",
-        },
-      })
-    end,
-    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
-  },
-  {
-    "nvim-neotest/neotest-python",
-  }
+    {
+        "linux-cultist/venv-selector.nvim",
+        cmd = "VenvSelect",
+        opts = function(_, opts)
+            opts.dap_enabled = true
+            return vim.tbl_deep_extend("force", opts, {
+                name = {
+                    "venv",
+                    ".venv",
+                    "env",
+                    ".env",
+                },
+            })
+        end,
+        keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+    },
+    {
+        "nvim-neotest/neotest-python",
+    }
 }
